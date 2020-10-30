38edf4fad1ee46a348c1ada6cb7ba336a8c24e9b
Mayank-09 <70089444+Mayank-09@users.noreply.github.com>
2020-10-30 15:03:28 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,106 @@
+const number1box = document.getElementById("number1");
+const number2box = document.getElementById("number2");
+const number3box = document.getElementById("number3");
+
+var score = 0
+var operator, number1, number2, number3
+
+function randomise() {
+
+number1 = Math.round(Math.random()*100)
+number2 = Math.round(Math.random()*100)
+
+if(number1<number2) {
+    var extra = number1
+    number1 = number2
+    number2 = extra
+}
+
+    operator = Math.round(Math.random()*4)
+
+    switch(operator) {
+        case 0 :
+        case 1 : number3 = number1 + number2
+        break
+        case 2 :  number3 = number1 - number2
+        break
+        case 3 :  number3 = number1 * number2
+        break
+        case 4 : number3 = Math.floor(number1/number2)
+        number1 = number2*number3
+        break
+    }
+
+    number1box.innerHTML = number1
+    number2box.innerHTML = number2
+    number3box.innerHTML = number3
+}
+
+const plus = document.getElementById("plus")
+const minus = document.getElementById("minus")
+const mul = document.getElementById("mul")
+const divide = document.getElementById("divide")
+
+randomise();
+plus.onclick = ()=>{
+if(operator == 1){
+        score++
+        randomise()
+}
+else{
+    location.href = "./gameover.html?score"+score;
+}
+}
+
+minus.onclick = ()=>{
+    if(operator == 2){
+            score++
+            randomise()
+    }
+    else{
+        location.href = "./gameover.html?score"+score;
+    }
+    }
+
+    mul.onclick = ()=>{
+        if(operator == 3){
+                score++
+                randomise()
+        }
+        else{
+            location.href = "./gameover.html?score"+score;
+        }
+        }
+
+       divide.onclick = ()=>{
+            if(operator == 4){
+                    score++
+                    randomise()
+            }
+            else{
+                location.href = "./gameover.html?score"+score;
+            }
+            }
+
+            var score = 0;
+            var time = 5;
+            var number1 = Math.round(Math.random() * 100);
+            var number2 = Math.round(Math.random() * 100);
+            var timerId;
+
+function startTimer() {
+  time = 5;
+  timer.innerHTML = time;
+  timerId = setInterval(() => {
+    time--;
+    if (time == 0) location.href = "./gameover.html?score=" + score;
+    timer.innerHTML = time;
+  }, 1000);
+}
+
+function resetTime(intervalId) {
+  clearInterval(intervalId);
+  startTimer();
+}
+
+startTimer();
